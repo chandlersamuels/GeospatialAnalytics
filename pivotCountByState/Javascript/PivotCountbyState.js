ed5abfd04ff9aa8591b997bedcccf2a37f7edccf
Chandler Samuels <chandler.samuels@colorado.edu150pocono>
2017-05-31 17:49:51 -0600

    added data variance function to deal with even color mapping

--- pivotCountByState/Javascript/PivotCountbyState.js
+++ pivotCountByState/Javascript/PivotCountbyState.js
@@ -1,24 +1,25 @@
-//colorize the map
-var income_domain = [1, 30, 100, 500, 21000, 120000]
+var income_domain = []
+var colorRange = 6
 
-var income_color = d3.scaleLinear()
-  .domain(income_domain)
-  .range(["rgb(239,243,255)","rgb(198,219,239)","rgb(158,202,225)","rgb(107,174,214)","rgb(49,130,189)","rgb(8,81,156)"]);
+//var income_domain = [1, 30, 100, 500, 21000, 120000]
 
 
 
 var legendText = ["120,000+", "120,000 - 21,000", "21,000 - 500", "500 - 100","100 - 30","30-1"];
 
 var pivotcountData = d3.map();
+var pivotcountDataArray= [];
 
 d3.queue()
   .defer(d3.json, "USbyState/USMap.topojson")
   .defer(d3.csv, "Data/pivotcountofbillState.csv", function(d) {
     if(isNaN(d.count)){
       pivotcountData.set(d.state, 0);
+      pivotcountDataArray.push(0)
     }
     else{
       pivotcountData.set(d.state, +d.count)//sets the key as the id plus converts string to int
+      pivotcountDataArray.push(+d.count)
     }
   })
   .await(ready)
@@ -26,6 +27,33 @@ d3.queue()
 
 function ready(error, data){
   if(error) throw error;
+
+  console.log(pivotcountData)
+
+  var max = d3.max(pivotcountDataArray, function(d) { return d;});
+  var min = d3.min(pivotcountDataArray, function(d) { return d;});
+
+  console.log(min)
+
+  income_domain = range(max, min, colorRange);
+
+  var scaletype = "liear"
+  var income_color = {}
+  var quantile = {}
+
+  if(scaletype == "linear")
+  {
+    income_color = d3.scaleLinear() //
+      .domain(income_domain)
+      .range(["rgb(239,243,255)","rgb(198,219,239)","rgb(158,202,225)","rgb(107,174,214)","rgb(49,130,189)","rgb(8,81,156)"]);
+  }
+  else
+  {
+    income_color = d3.scaleLog() //
+        .base(Math.E)
+        .domain(income_domain)
+        .range(["rgb(239,243,255)","rgb(198,219,239)","rgb(158,202,225)","rgb(107,174,214)","rgb(49,130,189)","rgb(8,81,156)"]);
+  }
 
   //USMap
   var usMap = topojson.feature(data, {
@@ -49,10 +77,6 @@ function ready(error, data){
     .attr("fill", function(d){
       var value = pivotcountData.get(d.properties.STUSPS);
       return (value != 0 ? income_color(value) : "red");
-
-
-
-
     })
     .on("mouseover", function(d) {
     	div.transition()
@@ -67,7 +91,7 @@ function ready(error, data){
            .duration(500)
            .style("opacity", 0);
     });
-}
+
 
 var legend = d3.select("svg.legend")
   .attr("width", 200)
@@ -89,3 +113,5 @@ var legend = d3.select("svg.legend")
     .attr("y",9)
     .attr("dy",".30em")
     .text(function(d){return d;});
+
+}
