5d61335ac4293a7e95cc8b6cedfa29f974eeedca
Chandler Samuels <chandler.samuels@colorado.edu150pocono>
2017-06-07 16:43:17 -0600

    Adding Shentel: pivot count by Ticket

---
+++ SlippyMap/Shentel/pivotcountbyTicket/Javascript/helper.js
@@ -0,0 +1,9 @@
+function range(max, min, colorRange){;
+    var temp = 0;
+    var diff = Math.round(max / colorRange);
+    var dataRange = [min];
+    for(i=1; i<colorRange; i++){
+        dataRange[i]= dataRange[i-1] + diff;
+      }
+    return dataRange;
+}
